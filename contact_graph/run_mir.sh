95752fe61b6ef0c707e8973a4670b1280cfd0e8e
mikessh <mikhail.shugay@gmail.com>
2023-04-11 09:32:10 +0300

    Update MIR and PDB annotations

--- contact_graph/run_mir.sh
+++ contact_graph/run_mir.sh
@@ -1,16 +1,14 @@
-MIR="java -Xmx20G -cp ../mir-1.0-SNAPSHOT.jar com.milaboratory.mir.scripts.Examples"
+MIR="java -Xmx24G -jar mir-1.0-SNAPSHOT.jar"
 
-DIR=$1
-
-mkdir $1/../out_mir 
-mkdir $1/../out_mir/log
+mkdir $1/summary/
+mkdir $1/summary/log/
 
 echo "Annotating structures"
-$MIR annotate-structures -I `ls -p $1/*.pdb` -O $1/../out_mir/ 2>&1 | tee $1/../out_mir/log/ann_log.txt
+$MIR annotate-structures -I `ls -p $1/*.pdb` -O $1/summary/ 2>&1 | tee $1/summary/log/ann_log.txt
 echo "Computing geometry"
-$MIR compute-pdb-geom -I `ls -p $1/*.pdb` -O $1/../out_mir/ 2>&1 | tee $1/../out_mir/log/geom_log.txt
+$MIR compute-pdb-geom -I `ls -p $1/*.pdb` -O $1/summary/ 2>&1 | tee $1/summary/log/geom_log.txt
 echo "Computing contacts"
-$MIR compute-pdb-contacts -I `ls -p $1/*.pdb` -O $1/../out_mir/ 2>&1 | tee $1/../out_mir/log/contact_log.txt
+$MIR compute-pdb-contacts -I `ls -p $1/*.pdb` -O $1/summary/ 2>&1 | tee $1/summary/log/contact_log.txt
 
-cd $1/../out_mir
+cd $1/summary/
 gzip *
